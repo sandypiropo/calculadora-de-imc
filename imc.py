7a28d0d85eddb726946c30a693380ebdb2b35075
Sandy Pirôpo <121200148+sandypiropo@users.noreply.github.com>
2023-02-27 23:26:10 -0300

    Update imc.py
    Melhora na parte gráfica versão 1

--- imc.py
+++ imc.py
@@ -4,13 +4,23 @@ class TelaPython:
     def __init__(self):
      # layout
         layout = [
-            [sg.Text('Peso'),sg.Input()],
-            [sg.Text('Altura'),sg.Input()],
+            [sg.Text('Digite seu peso e a sua altura: ')],
+            [sg.Text('Peso (Kg)', size=(5,0)),sg.Input(size=(25,0),key='peso')],
+            [sg.Text('Altura (M)', size=(5,0)),sg.Input(size=(25,0),key='alt')],
             [sg.Button('Calcular')]
         ]
-        janela = sg.Window('Calculadora de IMC').layout(layout)
-        self.button, self.values = janela.Read()
+        #Janela
+        self.janela = sg.Window('Calculadora de IMC').layout(layout)
+       
+    
     def Iniciar(self):
-        print(self.values)
+        while True:
+            # Extração de dados da tela
+            self.button, self.values = self.janela.Read()
+            peso = self.values['peso']
+            alt = self.values['alt']
+            print(f'peso: {peso}')
+            print(f'alt: {alt}')
+
 tela = TelaPython()
 tela.Iniciar()
